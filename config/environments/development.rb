6b540ff7cc1deff141107dadf6cf020022c53c99
Ubuntu <ubuntu@ip-172-31-31-30.us-west-1.compute.internal>
2022-01-01 00:15:06 +0000

    Add initial home and about page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "58ae7cf128f0475a888d70ad6bfd5e1a.vfs.cloud9.us-west-1.amazonaws.com"
 end
